efa1b24ef36249152e8eea629b8dfbf8f39b27d8
ChanghanGe <51274778+ChanghanGe@users.noreply.github.com>
2023-11-20 16:27:03 -0600

    Update generate_tx_codebook_16ant_random.py

--- codebook/generate_tx_codebook_16ant_random.py
+++ codebook/generate_tx_codebook_16ant_random.py
@@ -3,6 +3,7 @@
 #PI: Prof. Lili Qiu @ UT Austin/MSRA
 #Participants: Yiwen Song @ CMU, Changhan Ge @ UT Austin, Yin Zhang
 #Copyright @ The University of Texas at Austin, 2023
+#Partially inherited from Teng Wei/Song Wang/Jingqi Huang/Xinyu Zhang @ UCSD
 ####################################################################
 
 from random import randint
